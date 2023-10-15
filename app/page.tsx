64199cd82e968c8e102c243ee2510150c5401804
Vincenzo <vincenzopallante@icoud.com>
2023-10-15 18:00:05 +0200

    feat: new branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fuels, yearsOfProduction } from "@/constants";
+import { SearchParamsProp } from "@/types";
 import { fetchCars } from "@/utils";
-import Image from "next/image";
 
-export default async function Home({ searchParams }) {
+export default async function Home({ searchParams }: SearchParamsProp) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
